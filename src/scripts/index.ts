e86e26f7231d13cb338a19bec00678de221600cb
Tryd0g0lik <work80@mail.ru>
2024-11-16 13:55:10 +0700

    [master]: flask form added was

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,1 +1,22 @@
+/***
+ * There is a listener of download a page
+ */
+import { handlerEventSubmit } from "./services/handler/handlerFormSubmit";
 
+document.removeEventListener("DOMContentLoaded", handlerMain);
+document.addEventListener("DOMContentLoaded", handlerMain);
+
+function handlerMain(): boolean {
+  /***
+   * This is a function that is the main handler for all handlers. 
+   */
+  const formHtml = document.querySelector("smsForm");
+  if (formHtml === null) {
+    return false
+  }
+
+  (formHtml as HTMLFormElement).removeEventListener("mousedown", handlerEventSubmit);
+  (formHtml as HTMLFormElement).addEventListener("mousedown", handlerEventSubmit)
+
+  return true
+}
