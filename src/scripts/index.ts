11066bebfa4d774bfcd12b4f148a615cf6df39fa
Tryd0g0lik <work80@mail.ru>
2024-11-16 17:45:23 +0700

    [master]: completed

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,7 +1,7 @@
 /***
  * There is a listener of download a page
  */
-import { handlerEventSubmit } from "./services/handler/handlerFormSubmit";
+
 
 document.removeEventListener("DOMContentLoaded", handlerMain);
 document.addEventListener("DOMContentLoaded", handlerMain);
@@ -10,13 +10,5 @@ function handlerMain(): boolean {
   /***
    * This is a function that is the main handler for all handlers. 
    */
-  const formHtml = document.querySelector("smsForm");
-  if (formHtml === null) {
-    return false
-  }
-
-  (formHtml as HTMLFormElement).removeEventListener("mousedown", handlerEventSubmit);
-  (formHtml as HTMLFormElement).addEventListener("mousedown", handlerEventSubmit)
-
   return true
 }
